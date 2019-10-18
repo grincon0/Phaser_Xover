184f4c743cfd85d3ec04f5b8c7beec188b304679
grincon0 <georgerincon2@gmail.com>
2019-10-18 16:06:36 -0400

    working phaser canvas in react

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,36 @@
-import React, {Component} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
+import Phaser from 'phaser';
 
-class App extends Component {
-    render(){
+const config = {
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    console.log('pre-loading')
+    //game.scene.add.
+}
+function create() {
+    console.log('creating')
+
+}
+
+const App = () => {
+
         return(
         <div>
             <h1>Look ma, no create-react-app!</h1>
         </div>
         );
-    }
+    
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Phaser from 'phaser';
 import App from './components/App';
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
